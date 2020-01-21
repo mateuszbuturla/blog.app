87b5956bedde77a593bca7c849f20c7a14901f16
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-21 20:50:54 +0100

    create add post section

--- client/src/view/addPost/AddPost.js
+++ client/src/view/addPost/AddPost.js
@@ -3,11 +3,21 @@ import React from 'react';
 import Nav from '../nav/Nav';
 import Footer from '../footer/Footer';
 
+import './AddPost.css';
+
 const AddPost = () => {
     return (
         <React.Fragment>
             <Nav />
-            <h2>Add Post</h2>
+            <section className="add-post">
+                <h2 className="add-post__header">Add Post</h2>
+                <form>
+                    <input className="add-post__input" type="text" placeholder="Title" id="title" /><br />
+                    <input className="add-post__input" type="text" placeholder="Author" id="author" /><br />
+                    <textarea className="add-post__textarea" placeholder="Content" id="content"></textarea><br />
+                    <input className="add-post__submit" type="submit" value="Add post" />
+                </form>
+            </section>
             <Footer />
         </React.Fragment>
     );
