e36b18ec9b2cfba87975c161b11dae963d52707f
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-21 20:58:13 +0100

    create nav

--- client/src/view/nav/Nav.js
+++ client/src/view/nav/Nav.js
@@ -4,12 +4,13 @@ import './nav.css';
 
 const Nav = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/addpost">Add Post</Link></li>
-                <li><Link to="/posts">Posts</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+        <nav className="nav">
+            <p className="nav__header">Blog.app</p>
+            <ul className="nav__list">
+                <li><Link className="nav__link" to="/">Home</Link></li>
+                <li><Link className="nav__link" to="/addpost">Add Post</Link></li>
+                <li><Link className="nav__link" to="/posts">Posts</Link></li>
+                <li><Link className="nav__link" to="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
