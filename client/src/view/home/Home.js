06155d0e17cb9f3e2b24c3a90e64f0e41e84f21b
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-21 21:00:51 +0100

    create home

--- client/src/view/home/Home.js
+++ client/src/view/home/Home.js
@@ -3,11 +3,15 @@ import React from 'react';
 import Nav from '../nav/Nav';
 import Footer from '../footer/Footer';
 
+import './Home.css'
+
 const Home = () => {
     return (
         <React.Fragment>
             <Nav />
-            <h2>Posts</h2>
+            <section className="home">
+                <h2 className="home__header">Blog</h2>
+            </section>
             <Footer />
         </React.Fragment>
     );
