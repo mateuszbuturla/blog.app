1bdca7c7e8751bdc8d618fd6a891b595adeb963d
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-22 16:36:26 +0100

    add style to post

--- client/src/view/post/Post.js
+++ client/src/view/post/Post.js
@@ -8,9 +8,9 @@ const Posts = (props) => {
     return (
         <div className="post">
             <h2 className="post__title">{title}</h2>
-            <p className="post__author">{author}</p>
+            <p className="post__author">Author: {author}</p>
             <p className="post__content">{content}</p>
-            <Link to={`/post/${_id}`}>Read more</Link>
+            <Link className="post__read-more" to={`/post/${_id}`}>Read more</Link>
         </div>
     );
 }
