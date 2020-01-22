8ee52ea44c00bcecb06b53223c974053b3fe97b3
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-22 15:36:49 +0100

    create all posts section

--- client/src/view/posts/Posts.js
+++ client/src/view/posts/Posts.js
@@ -2,19 +2,40 @@ import React from 'react';
 
 import Nav from '../nav/Nav';
 import Footer from '../footer/Footer';
+import Post from '../post/Post';
 
 import './posts.css'
 
-const Posts = () => {
-    return (
-        <React.Fragment>
-            <Nav />
-            <section className="posts">
-                <h2 className="posts__header">Posts</h2>
-            </section>
-            <Footer />
-        </React.Fragment>
-    );
+
+class Posts extends React.Component {
+
+    state = {
+        posts: [],
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:3000/api/posts`, { method: 'GET' })
+            .then(r => r.json())
+            .then(r => {
+                console.log(r)
+                this.setState({ posts: r })
+            })
+    }
+
+    render() {
+        const { posts } = this.state;
+        const _posts = posts.map(post => <Post key={post._id} post={post} />)
+        return (
+            <React.Fragment>
+                <Nav />
+                <section className="posts">
+                    <h2 className="posts__header">Posts</h2>
+                    {this.state.posts.length > 0 && _posts}
+                </section>
+                <Footer />
+            </React.Fragment>
+        );
+    }
 }
 
 export default Posts;
